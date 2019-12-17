7167c8e090a17e59a14348448f60b11ca04d96ea
tab.zhou <tab.zhou@FFCNN271.farfetch.local>
2019-12-17 18:49:58 +0800

    modify var name

--- Category/DoublePoint/MaxCapacity.py
+++ Category/DoublePoint/MaxCapacity.py
@@ -5,19 +5,19 @@ class MaxCapacity(object):
         head_point = 0
         end_point = len(container)-1
         max_area = 0
-        area_lenth  = end_point - head_point
-        while area_lenth:
+        area_length = end_point - head_point
+        while area_length:
             height_head = container[head_point]
             height_end = container[end_point]
             height = min(height_head, height_end)
-            if max_area <= height * area_lenth:
-                max_area = height * area_lenth
+            if max_area <= height * area_length:
+                max_area = height * area_length
 
             if height_head >= height_end:
                 end_point -= 1
             else:
                 head_point += 1
-            area_lenth = end_point - head_point
+            area_length = end_point - head_point
         return max_area
 
 
