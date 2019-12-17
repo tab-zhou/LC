475833be2076c9d5793779cdd83780a08c240d31
tab.zhou <tab.zhou@FFCNN271.farfetch.local>
2019-12-17 18:48:37 +0800

    add AllSubset method

--- Category/Recursive/AllSubset.py
+++ Category/Recursive/AllSubset.py
@@ -1,6 +1,25 @@
+from itertools import product
+
+
 class AllSubset(object):
+    def __init__(self):
+        self.ext = []
 
-    @staticmethod
-    def get_all_subset(or_set: set) -> list[set]:
+    @classmethod
+    def get_all_subset(cls, or_set: set):
+        result = [set()]
+        ext = []
+        ext_list = []
+        while or_set != set():
+            ext.append(or_set.pop())
+            for j in ext:
+                for i in result:
+                    ext_list.append({i.add(j)})
+            result = result + ext_list
+        return result
 
-        pass
+
+test_case = [{1, 2}]
+test = AllSubset()
+for t_index in test_case:
+    print(test.get_all_subset(t_index))
